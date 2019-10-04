2754c6f66d8e127edd3f96c89d45b8ab125168ef
yeji <aa960923@naver.com>
2019-10-04 21:43:39 +0900

    second

--- app/src/main/java/com/example/calapp/LearnActivity.java
+++ app/src/main/java/com/example/calapp/LearnActivity.java
@@ -3,6 +3,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +27,8 @@ public class LearnActivity extends AppCompatActivity {
         final TextView txtWord1 = findViewById(R.id.txtWord1);
         final TextView txtWord2 = findViewById(R.id.txtWord2);
         final TextView txtWord3 = findViewById(R.id.txtWord3);
+        final TextView txtResult = findViewById(R.id.txtResult);
+        final EditText txtAnswer = findViewById(R.id.txtAnswer);
         final Button btnNext = findViewById(R.id.btnNext);
         final Button btnAns = findViewById(R.id.btnAns);
         final Button btnInit = findViewById(R.id.btnInit);
@@ -39,11 +42,26 @@ public class LearnActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (cnt < 5) {
                     cnt++;
-                    cal(txtWord1,txtWord2,txtWord3);
+                    int tmp = cal(txtWord1,txtWord2,txtWord3);
+                    int tmp2 = Integer.parseInt(txtAnswer.getText().toString());
+                    if(tmp==tmp2){
+                        txtResult.setText("정답입니다.");
+                    }else{
+                        txtResult.setText("틀렸습니다.");
+                    }
+
 
                 } else {
                     Toast.makeText(LearnActivity.this, "마지막 문제입니다.", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        btnAns.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
             }
         });
 
@@ -57,11 +75,11 @@ public class LearnActivity extends AppCompatActivity {
 
     }
 
-    public static void cal(TextView txtWord1,TextView txtWord2,TextView txtWord3){
+    public static int cal(TextView txtWord1,TextView txtWord2,TextView txtWord3){
         Random ran = new Random();
 
         int num1 = ran.nextInt(300) + 100;
-        int num2 = ran.nextInt(299) + 1;
+        int num2 = ran.nextInt(199) + 1;
         int sign = ran.nextInt(2);
 
         txtWord1.setText(""+num1);
@@ -71,6 +89,8 @@ public class LearnActivity extends AppCompatActivity {
             txtWord2.setText("-");
         }
         txtWord3.setText(""+num2);
+
+        return num1+num2;
     }
 
 }
